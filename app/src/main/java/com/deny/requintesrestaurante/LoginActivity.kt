f35381a2201cc70251ead86d159d898b573af50e
Bruno Brito <batioe.std@gmail.com>
2021-11-08 20:11:00 -0300

    pequenas mudancas

--- app/src/main/java/com/deny/requintesrestaurante/LoginActivity.kt
+++ app/src/main/java/com/deny/requintesrestaurante/LoginActivity.kt
@@ -1,22 +1,152 @@
 package com.deny.requintesrestaurante
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.isInvisible
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.*
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var button: Button
+    var             firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    lateinit var     buttonLogin: Button
+    lateinit var buttonCadastrar: Button
+    lateinit var     recebeEmail: EditText
+    lateinit var     recebeSenha: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
 
-        button = findViewById(R.id.button2)
+        buttonLogin      = findViewById(R.id.buttonEntrar)
+        buttonCadastrar  = findViewById(R.id.buttonCadastrar)
+        recebeEmail      = findViewById(R.id.editTextLoginEmail)
+        recebeSenha      = findViewById(R.id.editTextLoginSenha)
 
-        button.setOnClickListener(View.OnClickListener {
+        buttonCadastrar.isInvisible = true
+
+        /*buttonCadastrar.setOnClickListener(View.OnClickListener {
+            cadastrar()
+        })*/
+
+        buttonLogin.setOnClickListener(View.OnClickListener {
+            login()
         })
     }
+
+    fun login(){
+        if (!recebeEmail.text.toString().isEmpty()){
+            if (!recebeSenha.text.toString().isEmpty()){
+
+                var auxEmail: String = recebeEmail.text.toString()
+                var auxSenha: String = recebeSenha.text.toString()
+
+                firebaseAuth.signInWithEmailAndPassword(auxEmail, auxSenha).addOnCompleteListener(
+                    OnCompleteListener {
+                        if (it.isSuccessful){
+                            Toast.makeText(
+                                this,
+                                "Login feito com sucesso",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            val intent2 = Intent(this, CardapioActivity::class.java)
+                            startActivity(intent2)
+                            finish()
+
+                        } else {
+                            var excecao = ""
+                            try {
+                                throw it.exception!!
+                            } catch (e: FirebaseAuthInvalidCredentialsException) {
+                                excecao = "E-mail ou senha não corresponde a um usuario cadastrado"
+                            } catch (e: FirebaseAuthInvalidUserException) {
+                                excecao = "Usuario não está cadastrado"
+                            } catch (e: Exception) {
+                                excecao = "Erro ao fazer login" + e.message
+                                e.printStackTrace()
+                            }
+                            Toast.makeText(
+                                this,
+                                excecao,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
+
+            }else{
+                Toast.makeText(
+                    this,
+                    "Digite alguma senha",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else{
+            Toast.makeText(
+                this,
+                "Digite algum email",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    /*fun cadastrar(){
+        if (!recebeEmail.text.toString().isEmpty()){
+            if (!recebeSenha.text.toString().isEmpty()){
+
+                var auxEmail: String = recebeEmail.text.toString()
+                var auxSenha: String = recebeSenha.text.toString()
+
+                firebaseAuth.createUserWithEmailAndPassword(auxEmail, auxSenha).addOnCompleteListener(
+                    OnCompleteListener {
+                        if (it.isSuccessful){
+                            Toast.makeText(
+                                this,
+                                "Cadastrado com sucesso",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            var excecao = ""
+                            try {
+                                throw it.exception!!
+                            } catch (e: FirebaseAuthWeakPasswordException) {
+                                excecao = "Digite uma senha mais forte"
+                            } catch (e: FirebaseAuthInvalidCredentialsException) {
+                                excecao = "Digite um e-mail válido"
+                            } catch (e: FirebaseAuthUserCollisionException) {
+                                excecao = "Este usuario já existe"
+                            } catch (e: Exception) {
+                                excecao = "Erro ao cadastrar usuario" + e.message
+                                e.printStackTrace()
+                            }
+                            Toast.makeText(
+                                this,
+                                excecao,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    })
+
+            } else{
+                Toast.makeText(
+                    this,
+                    "Digite uma senha",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else{
+            Toast.makeText(
+                this,
+                "Digite um email",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }*/
 }
---
+++ app/src/main/java/com/deny/requintesrestaurante/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,45 @@
+package com.deny.requintesrestaurante.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.deny.requintesrestaurante.R
+import com.deny.requintesrestaurante.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
